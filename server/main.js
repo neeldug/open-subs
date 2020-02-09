8c74fc239cf8b1ae17e0b1e5542602db1105ffee
nd419 <nd419@imperial.ac.uk>
2020-02-09 14:20:00 +0000

    Some things with speech rec

--- server/main.js
+++ server/main.js
@@ -2,4 +2,45 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  asyncRecognise();
 });
+
+async function asyncRecognise() {
+    const fs = require('fs');
+
+    // Imports the Google Cloud client library
+    const speech = require('@google-cloud/speech');
+
+    // Creates a client
+    const client = new speech.SpeechClient();
+
+    /**
+     * TODO(developer): Uncomment the following lines before running the sample.
+     */
+    const filename = '/Users/neel/open-subs/public/philo.mp3';
+    const encoding = 'MP3';
+    const sampleRateHertz = 44100;
+    const languageCode = 'en-GB';
+
+    const request = {
+      config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+      },
+      interimResults: false, // If you want interim results, set this to true
+    };
+
+    // Stream the audio to the Google Cloud Speech API
+    const recognizeStream = client
+      .streamingRecognize(request)
+      .on('error', console.error)
+      .on('data', data => {
+        console.log(
+          `Transcription: ${data.results[0].alternatives[0].transcript}`
+        );
+      });
+
+    // Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
+    fs.createReadStream(filename).pipe(recognizeStream);
+}
---
+++ starter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# METEOR_SETTINGS='{"GOOGLE_APPLICATION_CREDENTIALS": "private/open-subs-a55498aab702.json"}' meteor
+
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/neel/open-subs/private/open-subs-a55498aab702.json"
+
+meteor
