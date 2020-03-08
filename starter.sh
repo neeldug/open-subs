cc73cb3f8526380f45f46f90a0bf0dc02576579b
nd419 <nd419@imperial.ac.uk>
2020-03-08 15:49:52 +0000

    plyr install & ffmpeg --find alternatives for python example in node

--- starter.sh
+++ starter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # METEOR_SETTINGS='{"GOOGLE_APPLICATION_CREDENTIALS": "private/open-subs-a55498aab702.json"}' meteor
 
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/neel/open-subs/private/open-subs-a55498aab702.json"
-
+export AWS_ACCESS_KEY_ID="AKIA5MFVB2Z6ZOESQPET"
+export AWS_SECRET_ACCESS_KEY="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"
 meteor
